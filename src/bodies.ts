94b12969948c80a0b90afa7747b62062006d34c9
Thomas Yeung <yeung.thomas.h@gmail.com>
2025-02-28 21:49:03 -0800

    thingy

--- src/bodies.ts
+++ src/bodies.ts
@@ -11,24 +11,175 @@
  */
 
 class Body {
-    draw() {
+    color: string;
+    velX: number;
+    velY: number;
 
+    constructor(color: string) {
+        this.color = color;
+        this.velX = 0, this.velY = 0;
     }
 
-    collision() {
+    draw() {
+        throw new Error("draw() must be implemented in subclass");
+    }
+
+    move(speed: number) {
+        throw new Error("move() must be implemented in subclass");
+    }
+
+    detectCollision(other: Body) {
+        throw new Error("detectCollision() must be implemented in subclass");
 
     }
 }
 
 class Rect extends Body {
-    point: { x: number, y: number };
+    center: { x: number, y: number };
     width: number;
     height: number;
+    rotation: number;
 
-    constructor(point: any, width: number, height: number) {
-        super();
-        this.point = point;
+    constructor(color: string, center: { x: number, y: number }, width: number, height: number, rotation=0) {
+        super(color);
+        this.center = center;
         this.width = width;
         this.height = height;
+        this.rotation = rotation;
+    }
+
+    draw(): void {
+        let p1 = rotation(this.center, {x: this.center.x-this.width/2, y: this.center.y-this.height/2}, this.rotation);
+        let p2 = rotation(this.center, {x: this.center.x+this.width/2, y: this.center.y-this.height/2}, this.rotation);
+        let p3 = rotation(this.center, {x: this.center.x+this.width/2, y: this.center.y+this.height/2}, this.rotation);
+        let p4 = rotation(this.center, {x: this.center.x-this.width/2, y: this.center.y+this.height/2}, this.rotation);
+    }
+
+    move(speed: number) {
+        this.center.x += Math.cos(this.rotation)*speed;
+        this.center.y += Math.sin(this.rotation)*speed;
+    }
+
+    rotate(amount: number) {
+        this.rotation += amount;
+    }
+
+    detectCollision(other: Body): boolean {
+        let p1 = rotation(this.center, {x: this.center.x-this.width/2, y: this.center.y-this.height/2}, this.rotation);
+        let p2 = rotation(this.center, {x: this.center.x+this.width/2, y: this.center.y-this.height/2}, this.rotation);
+        let p3 = rotation(this.center, {x: this.center.x+this.width/2, y: this.center.y+this.height/2}, this.rotation);
+        let p4 = rotation(this.center, {x: this.center.x-this.width/2, y: this.center.y+this.height/2}, this.rotation);
+
+        function detectCollisionRect(other: Rect): boolean {
+            return false;
+        }
+    
+        function detectCollisionCircle(other: Circle): boolean {
+            return false;
+        }
+    
+        function detectCollisionTriangle(other: Triangle): boolean {
+            return false;
+        }
+
+        if (other instanceof Rect) return detectCollisionRect(other);
+        if (other instanceof Circle) return detectCollisionCircle(other);
+        if (other instanceof Triangle) return detectCollisionTriangle(other);
+    }
+}
+
+class Circle extends Body {
+    point: { x: number, y: number };
+    radius: number;
+    rotation: number;
+
+    constructor(color: string, point: { x: number, y: number }, radius: number, rotation=0) {
+        super(color);
+        this.point = point;
+        this.radius = radius;
+        this.rotation = rotation;
+    }
+
+    draw() {
+
+    }
+
+    move(speed: number) {
+        this.point.x += Math.cos(this.rotation)*speed;
+        this.point.y += Math.sin(this.rotation)*speed;
+    }
+
+    rotate(amount: number) {
+        this.rotation += amount;
+    }
+
+    detectCollision(other: Body): boolean {
+        function detectCollisionRect(other: Rect): boolean {
+            return false;
+        }
+    
+        function detectCollisionCircle(other: Circle): boolean {
+            return false;
+        }
+    
+        function detectCollisionTriangle(other: Triangle): boolean {
+            return false;
+        }
+
+        if (other instanceof Rect) return detectCollisionRect(other);
+        if (other instanceof Circle) return detectCollisionCircle(other);
+        if (other instanceof Triangle) return detectCollisionTriangle(other);
+    }
+}
+
+class Triangle extends Body {
+    center: { x: number, y: number };
+    width: number;
+    height: number;
+    rotation: number;
+
+    constructor(color: string, center: { x: number, y: number }, width: number, height: number, rotation=0) {
+        super(color);
+        this.center = center;
+        this.width = width;
+        this.height = height;
+        this.rotation = rotation;
+    }
+
+    draw() {
+        let p1 = rotation(this.center, {x: this.center.x+this.width/2, y: this.center.y}, this.rotation);
+        let p2 = rotation(this.center, {x: this.center.x-this.width/2, y: this.center.y+this.height/2}, this.rotation);
+        let p3 = rotation(this.center, {x: this.center.x-this.width/2, y: this.center.y-this.height/2}, this.rotation);
+    }
+
+    move(speed: number) {
+        this.center.x += Math.cos(this.rotation)*speed;
+        this.center.y += Math.sin(this.rotation)*speed;        
+    }
+
+    rotate(amount: number) {
+        this.rotation += amount;
+    }
+
+    detectCollision(other: Body): boolean {
+        let p1 = rotation(this.center, {x: this.center.x+this.width/2, y: this.center.y}, this.rotation);
+        let p2 = rotation(this.center, {x: this.center.x-this.width/2, y: this.center.y+this.height/2}, this.rotation);
+        let p3 = rotation(this.center, {x: this.center.x-this.width/2, y: this.center.y-this.height/2}, this.rotation);
+
+        function detectCollisionRect(other: Rect): boolean {
+            return false;
+        }
+    
+        function detectCollisionCircle(other: Circle): boolean {
+            return false;
+        }
+    
+        function detectCollisionTriangle(other: Triangle): boolean {
+            return false;
+        }
+
+        if (other instanceof Rect) return detectCollisionRect(other);
+        if (other instanceof Circle) return detectCollisionCircle(other);
+        if (other instanceof Triangle) return detectCollisionTriangle(other);
     }
 }
---
+++ src/helper.ts
@@ -0,0 +1,8 @@
+function rotation(origin: {x: number, y: number}, point: {x: number, y: number}, rotation: number): {x: number, y: number} {
+    let relX = point.x - origin.x, relY = point.y - origin.y;
+
+    return {
+        x: relX*Math.cos(rotation)-relY*Math.sin(rotation) + origin.x,
+        y: relX*Math.sin(rotation)+relY*Math.cos(rotation) + origin.y
+    };
+}
