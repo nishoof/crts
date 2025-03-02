194f413521df85ccd92caab9da9ba9007688fc92
Leo Zhang <leozhang2024@gmail.com>
2025-03-01 16:29:57 -0800

    changed some stuff

--- src/spawnables.ts
+++ src/spawnables.ts
@@ -2,15 +2,20 @@ import { Shape } from "./shapes";
 
 class Orb{
     health: number;
-    shape: Shape;
+    exp: number;
     rotationSpeed: number;
-    constructor(health: number, shape: Shape, rotationSpeed: number) {
+    shape: Shape;
+    constructor(health: number, exp: number, rotationSpeed: number, shape: Shape) {
         this.health = health;
-        this.shape = shape;
+        this.exp = exp;
         this.rotationSpeed = rotationSpeed;
+        this.shape = shape;
     }
     draw(ctx: CanvasRenderingContext2D, topLeft: {x: number, y: number}) {
         this.shape.rotation += this.rotationSpeed;
         this.shape.draw(ctx, topLeft);
+    }
+    updateHealth(damage: number) {
+        this.health -= damage;
     }
 }
