f1b9d4d976e128c60ff33aea10eb8a14b6ff49bb
Thomas Yeung <yeung.thomas.h@gmail.com>
2025-03-01 18:15:48 -0800

    hud cahnges

--- src/hud.ts
+++ src/hud.ts
@@ -2,9 +2,18 @@ import Player from "./player/player.js";
 
 function drawHUD(ctx: CanvasRenderingContext2D, player: Player) {
     // upgrades
+    const upgradeableVehicles = ["Bike", "Car", "Moped", "Hoverboard"];
     if (player.level >= player.vehicle.levelUp) {
         document.getElementById("vehicle-upgrades")!.style.display = "block";
-        document.getElementById(`${player.vehicle.name}-upgrades`)!.style.display = "flex";
+        upgradeableVehicles.forEach((vehicleName) => {
+            if (vehicleName == player.vehicle.name) document.getElementById(`${vehicleName}-upgrades`)!.style.display = "flex";
+            else document.getElementById(`${vehicleName}-upgrades`)!.style.display = "none";
+        });
+    } else {
+        document.getElementById("vehicle-upgrades")!.style.display = "none";
+        upgradeableVehicles.forEach((vehicleName) => {
+            document.getElementById(`${vehicleName}-upgrades`)!.style.display = "none";
+        });
     }
     // if (player.level > player.character.levelUp) {
     //     document.getElementById("character-upgrades")!.style.display = "block";
@@ -12,5 +21,6 @@ function drawHUD(ctx: CanvasRenderingContext2D, player: Player) {
     // }
 
     // bottom display
-    document.getElementById("level-progress-bar")!.style.width = "50%"; // update based on player level
+    document.getElementById("level-progress-text")!.innerHTML = `Level ${player.level}   ${player.progressToNextLevel}/${(player.level+1)*100}`;
+    document.getElementById("level-progress-bar")!.style.width = `${player.progressToNextLevel/(player.level+1)}%`;
 }
