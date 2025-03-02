2da3fb7454e8951c7237b13efe2f672fd7af789e
Thomas Yeung <yeung.thomas.h@gmail.com>
2025-03-01 16:33:56 -0800

    small hud update

--- src/hud.ts
+++ src/hud.ts
@@ -4,7 +4,7 @@ function drawHUD(ctx: CanvasRenderingContext2D, player: Player) {
     // upgrades
     if (player.level >= player.vehicle.levelUp) {
         document.getElementById("vehicle-upgrades")!.style.display = "block";
-        document.getElementById(`{player.vehicle.name}-upgrades`)!.style.display = "flex";
+        document.getElementById(`${player.vehicle.name}-upgrades`)!.style.display = "flex";
     }
     // if (player.level > player.character.levelUp) {
     //     document.getElementById("character-upgrades")!.style.display = "block";
