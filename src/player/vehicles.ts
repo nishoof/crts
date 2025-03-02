c2b26ec06f63e6a63ae857d797a746be80064483
Thomas Yeung <yeung.thomas.h@gmail.com>
2025-03-01 16:10:36 -0800

    updated stats on vehicles

--- src/player/vehicles.ts
+++ src/player/vehicles.ts
@@ -51,7 +51,7 @@ export class Bike extends Vehicle {
 
 export class Car extends Vehicle {
     constructor(position: { x: number, y: number }) {
-        super("Car", new Rect("blue", position, 80, 40), 0.3, 3, 0.02, 150);
+        super("Car", new Rect("blue", position, 80, 40), 0.03, 6, 0.02, 150);
         this.possibleEvolutions = ["Truck", "Racecar"];
         this.levelUp = 30;
     }
@@ -59,19 +59,19 @@ export class Car extends Vehicle {
 
 export class Truck extends Vehicle {
     constructor(position: { x: number, y: number }) {
-        super("Truck", new Rect("grey", position, 50, 50), 0.1, 2.5, 0.01, 250);
+        super("Truck", new Rect("grey", position, 50, 50), 0.02, 5.5, 0.01, 250);
     }
 }
 
 export class Racecar extends Vehicle {
     constructor(position: { x: number, y: number }) {
-        super("Racecar", new Triangle("green", position, 50, 50), 0.5, 4, 0.02, 120);
+        super("Racecar", new Triangle("green", position, 50, 50), 0.05, 8, 0.02, 120);
     }
 }
 
 export class Moped extends Vehicle {
     constructor(position: { x: number, y: number }) {
-        super("Moped", new Triangle("blue", position, 50, 44), 0.35, 2.75, 0.35, 100);
+        super("Moped", new Triangle("blue", position, 50, 44), 0.035, 5, 0.35, 100);
         this.possibleEvolutions = ["Motorcycle"];
         this.levelUp = 30;
     }
@@ -79,13 +79,13 @@ export class Moped extends Vehicle {
 
 export class Motorcycle extends Vehicle {
     constructor(position: { x: number, y: number }) {
-        super("Motorcycle", new Rect("red", position, 60, 10), 0.35, 3.5, 0.35, 115);
+        super("Motorcycle", new Rect("red", position, 60, 10), 0.04, 6, 0.35, 115);
     }
 }
 
 export class Hoverboard extends Vehicle {
     constructor(position: { x: number, y: number }) {
-        super("Hoverboard", new Circle("white", position, 50, 50), 0.3, 3.5, 0.5, 90);
+        super("Hoverboard", new Circle("white", position, 50, 50), 0.04, 4, 0.5, 90);
         this.possibleEvolutions = ["Cybertruck", "UFO"];
         this.levelUp = 30;
     }
@@ -93,12 +93,12 @@ export class Hoverboard extends Vehicle {
 
 export class Cybertruck extends Vehicle {
     constructor(position: { x: number, y: number }) {
-        super("Cybertruck", new Rect("gray", position, 100, 50), 0.3, 4, 0.1, 200);
+        super("Cybertruck", new Rect("gray", position, 100, 50), 0.05, 6, 0.1, 200);
     }
 }
 
 export class UFO extends Vehicle {
     constructor(position: { x: number, y: number }) {
-        super("UFO", new Circle("green", position, 75, 75), 0.4, 4, 0.5, 140);
+        super("UFO", new Circle("green", position, 75, 75), 0.05, 5, 0.5, 140);
     }
 }
