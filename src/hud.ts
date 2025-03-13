2fa48c5ebfd3a04e3adc9814726da87a32a3a16f
Thomas Yeung <yeung.thomas.h@gmail.com>
2025-03-13 14:43:13 -0700

    Cleanup of unnecessary script, organized & fixed hud.ts

--- src/hud.ts
+++ src/hud.ts
@@ -1,36 +1,48 @@
 import Player from "./player/player.js";
 
-export function drawHUD(player: Player) {
-    let showingVehicleMenu = false;
-    // upgrades
+function drawVehicleMenu(player: Player): boolean {
+    // set displays to none initially
     const upgradeableVehicles = ["Bike", "Car", "Moped", "Hoverboard"];
-    if (player.level >= player.vehicle.levelUp) {
-        showingVehicleMenu = true;
-        document.getElementById("vehicle-upgrades")!.style.display = "block";
-        upgradeableVehicles.forEach((vehicleName) => {
-            if (vehicleName == player.vehicle.name) document.getElementById(`${vehicleName}-upgrades`)!.style.display = "flex";
-            else document.getElementById(`${vehicleName}-upgrades`)!.style.display = "none";
-        });
-    } else {
-        document.getElementById("vehicle-upgrades")!.style.display = "none";
-        upgradeableVehicles.forEach((vehicleName) => {
-            document.getElementById(`${vehicleName}-upgrades`)!.style.display = "none";
-        });
-    }
+    document.getElementById("vehicle-upgrades")!.style.display = "none";
+    upgradeableVehicles.forEach((vehicleName) => {
+        document.getElementById(`${vehicleName}-upgrades`)!.style.display = "none";
+    });
 
+    if (player.vehicle.levelUp == -1) return false; // max level
+    if (player.level < player.vehicle.levelUp) return false;
+
+    // show valid upgrades
+    document.getElementById("vehicle-upgrades")!.style.display = "block";
+    upgradeableVehicles.forEach((vehicleName) => {
+        if (vehicleName == player.vehicle.name) document.getElementById(`${vehicleName}-upgrades`)!.style.display = "flex";
+        else document.getElementById(`${vehicleName}-upgrades`)!.style.display = "none";
+    });
+
+    return true;    
+}
+
+function drawCharacterMenu(player: Player) {
+    // set displays to none initially
     const upgradeableCharacters = ["Rifleman", "Gunner", "Sniper", "Cannoneer"];
-    if (!showingVehicleMenu && player.level >= player.character.levelUp) {
-        document.getElementById("character-upgrades")!.style.display = "block";
-        upgradeableCharacters.forEach((characterName) => {
-            if (characterName == player.character.name) document.getElementById(`${characterName}-upgrades`)!.style.display = "flex";
-            else document.getElementById(`${characterName}-upgrades`)!.style.display = "none";
-        });
-    } else {
-        document.getElementById("character-upgrades")!.style.display = "none";
-        upgradeableCharacters.forEach((characterName) => {
-            document.getElementById(`${characterName}-upgrades`)!.style.display = "none";
-        });
-    }
+    document.getElementById("character-upgrades")!.style.display = "none";
+    upgradeableCharacters.forEach((characterName) => {
+        document.getElementById(`${characterName}-upgrades`)!.style.display = "none";
+    });
+    
+    if (player.character.levelUp == -1) return; // max level
+    if (player.level < player.character.levelUp) return;
+
+    // show valid upgrades
+    document.getElementById("character-upgrades")!.style.display = "block";
+    upgradeableCharacters.forEach((characterName) => {
+        if (characterName == player.character.name) document.getElementById(`${characterName}-upgrades`)!.style.display = "flex";
+        else document.getElementById(`${characterName}-upgrades`)!.style.display = "none";
+    }); 
+}
+
+export function drawHUD(player: Player) {
+    // upgrade menu
+    if (!drawVehicleMenu(player)) drawCharacterMenu(player);
 
     // bottom display
     document.getElementById("level-progress-text")!.innerHTML = `Level ${player.level}   ${player.progressToNextLevel}/${(player.level + 1) * 50}`;
